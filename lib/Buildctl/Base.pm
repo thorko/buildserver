f825d55f20f79111c9d85da7d06601b42ede3428
Thorko <thorko@thorko.de>
2017-05-17 14:36:27 +0200

    added first function

--- lib/Buildctl/Base.pm
+++ lib/Buildctl/Base.pm
@@ -9,6 +9,38 @@ sub new {
 	my $self = bless {}, $pkg;
 
 	return $self;
-
 }
 
+
+sub list_versions {
+    my $self = shift;
+    my $app = shift;
+    my $cc = $self->config;
+    $ll->debug("list versions");
+    if ( $app eq "" || $app eq "all") {
+       foreach (@apps) {
+            if ( ! -d "$cc->{'install_path'}/$_" ) {
+                print "$cc->{'install_path'}/$_ does not exist.\n";
+            } else {
+                my @v = glob "$cc->{'install_path'}/$_/[0-9].*";
+                print "$_: ";
+                foreach (@v) {
+                    $_ =~ s{.*/}{};
+                    print " $_ ";
+                }
+                print "\n";
+            }
+       }
+    } else {
+       $ll->debug("list version of $app");
+       my @v = glob "$cc->{'install_path'}/$app/[0-9].*";
+       print "$app: ";
+       foreach (@v) {
+            $_ =~ s{.*/}{};
+            print " $_ ";
+       }
+       print "\n";
+   }
+}
+
+1;
